360804c48a17041de52ffedaff01c13605d01771
YarikTheMashine <49390094+YarikTheMashine@users.noreply.github.com>
2020-05-22 07:37:39 +0300

    update

--- server.js
+++ server.js
@@ -34,18 +34,23 @@ app.use(function (err, req, res, next) {
 app.get("/ErrorExample", function (req, res, next) {
   next(new Error("Random error!"));
 });
+
 app.get("/api/articles", function (req, res) {
   res.send("This is not implemented now");
 });
+
 app.post("/api/articles", function (req, res) {
   res.send("This is not implemented now");
 });
+
 app.get("/api/articles/:id", function (req, res) {
   res.send("This is not implemented now");
 });
+
 app.put("/api/articles/:id", function (req, res) {
   res.send("This is not implemented now");
 });
+
 app.delete("/api/articles/:id", function (req, res) {
   res.send("This is not implemented now");
 });
